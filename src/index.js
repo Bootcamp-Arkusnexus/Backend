aa36fd322c4ae544cb8d21caee7b8237a35a66b6
Eduardo RS <erendon@arkusnexus.com>
2025-01-22 21:58:51 -0800

    refactor(users): improve CRUD API with database integration - Move endpoint logic to dedicated controllers - Update PUT, POST, GET, DELETE endpoints to interact with the database using client fields - Add .env file to manage database connection credentials

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-import express from 'express'; // Best practice
+import express from 'express';
 import { PORT } from './config.js';
 import userRoutes from './routes/user.routes.js'
-
+import morgan from 'morgan'; // Visualize HTTP REQUESTS in the console
 const app = express();
 
+// 
+app.use(morgan('dev'));
 app.use(express.json())
 app.get('/',(req, res) => {
     res.send('Hello World! using Express JS')
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,82 +1,25 @@
 import { Router } from "express";
-import { pool } from "../db.js";
+import { getUsers, getUserByID, createUser, updateUser, deleteUser } from "../controllers/users.controllers.js";
 
 const router = Router();
 
 // CRUD
 
-// Route for users
-
 // Create users
-router.post("/users", async (req, res) => {
-  const data = req.body;
-  console.log(data);
-  const { rows } = await pool.query(
-    "INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *",
-    [data.name, data.email]
-  );
-  //   console.log(rows);
-
-  //   res.send(`User: ${data.name}, ${data.email} was created`);
-  res.json(rows); //
-});
+router.post("/users", createUser);
 
 // Read list of users
-router.get("/users", async (req, res) => {
-  const { rows } = await pool.query("SELECT * FROM users");
-  //   console.log(rows);
-  res.json(rows);
-  //   res.send("Getting users");
-});
+router.get("/users", getUsers);
+// SELECT FROM user name, email where id = 1;
 
 // Route for users with id
 // Read just one user with ID
-router.get("/users/:userID", async (req, res) => {
-  const { userID } = req.params;
-  const { rows } = await pool.query(`SELECT * FROM users WHERE id = ${userID}`);
-  //   const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [
-  //     userID,
-  //   ]);
-  //   res.send(`Get user id: ${userID}`);
-  // When client get an ID that doesn't exist
-  if (rows.length === 0) {
-    return res.status(404).json({ message: "User not found or doens't exist" });
-  }
-  res.json(rows[0]);
-});
+router.get("/users/:userID", getUserByID);
 
 // Update user using user ID
-router.put("/users/:userID", async (req, res) => {
-  const { userID } = req.params;
-  const data = req.body;
-  const { rows } = await pool.query(
-    "UPDATE users SET name = $1, email = $2 WHERE id = $3 RETURNING *",
-    [data.name, data.email, userID]
-  );
-
-  //   console.log(rows);
-  //   console.log(result);
-  //   res.send(`Updating user id: ${userID}`);
-
-  // Return to Client
-  return res.json(rows[0]);
-});
+router.put("/users/:userID", updateUser);
 
 //Delete User
-router.delete("/users/:userID", async (req, res) => {
-  const { userID } = req.params;
-  //   const { rows } = await pool.query(`DELETE FROM * users WHERE id = ${userID}`);
-  const { rows, rowCount } = await pool.query(
-    `DELETE FROM users WHERE id = ${userID} RETURNING *`
-  );
-  console.log(rows);
-
-  if (rowCount.length === 0) {
-    return res.status(404).json({ message: "User not found or doens't exist" });
-  } else {
-    return res.json({ message: `User deleted ${rows}` });
-  }
-  //   res.send(`Delete user id: ${userID}`);
-});
+router.delete("/users/:userID", deleteUser);
 
 export default router;
